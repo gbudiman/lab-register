9d0ae042589d8a1848e54891d586b3f1594a6545
wahyu.g <wahyu.g@42ab024f-b9ea-726a-ad9f-d7546ec773e2>
2011-01-19 18:42:41 +0000

    Completed lab 3 coding

---
+++ trunk/source/debug4.vhd
@@ -0,0 +1,49 @@
+-- Purpose: show Iteration Limit Reached; Zero Delay Oscillation Error.
+-- Created 01/17/03
+-- Mohammed Abu Al Soud
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+entity debug4 is
+  port ( 
+	reset:	in STD_LOGIC     
+       );
+end debug4;
+
+architecture behavioral of debug4 is
+
+  
+signal sel, output: STD_LOGIC;
+ begin
+ --SEL <= '0' WHEN RESET = '1' OR OUTPUT = '0' ELSE '1';
+ --OUTPUT <= '0' WHEN SEL = '0' ELSE '1';
+ 
+init : process (reset)
+  begin
+	  if (reset='1') then
+	  	sel<='0';
+	  end if;
+  end process; 
+  
+ update1 : process (sel)
+  begin
+	  if (sel='0') then
+	  	output<='0';
+	  else
+	  	output<='1';
+	  end if;
+  end process; 
+  
+--  update2 : process (output)
+--  begin
+--	if (output='0') then
+--		sel<='1';
+--	else
+--		sel<='0';
+--	end if;
+--  end process;
+end behavioral;
+
